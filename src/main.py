Fix loss calculation

src/main.py
@@ -9,7 +9,7 @@ from datasets import load_dataset
 from datasets.arrow_dataset import Example, Batch
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from accelerate import Accelerator
-from torchmetrics import MeanMetric
+from torchmetrics import MeanMetric, Accuracy
 from tqdm import tqdm, trange
 
 
@@ -18,13 +18,16 @@ def main():
     config = Config(
         project_name = "llm-peft",
         model_name = "tiiuae/falcon-7b",
-        n_training_iterations = 200,
+        n_training_iterations = 3000,
         effective_batch_size = 2,
-        learning_rate = 1e-4,
         gradient_accumulation_steps = 1,
+        learning_rate = 2e-4,
+        lora_rank = 16,
+        lora_alpha = 1.0,
+        lora_dropout = 0.1,
         max_sequence_length = 512,
         log_every_n_steps = 10,
-        validate_every_n_steps = 50,
+        validate_every_n_steps = 200,
     )
 
     # data preparation
@@ -32,32 +35,41 @@ def main():
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "left"
 
-    def tokenize(data: Example | Batch) -> BatchEncoding:
-        return tokenizer(
-            data["text"],
+    negative_token_id = tokenizer.encode("Negative")[0]
+    positive_token_id = tokenizer.encode("Positive")[0]
+    label_indices = torch.tensor([negative_token_id, positive_token_id], dtype=torch.int32)
+
+    def preprocess(data: Example | Batch) -> BatchEncoding:
+        labeled_reviews = []
+        for review, label in zip(data["text"], iter(data["label"])):
+            label_text = "Positive" if label.item() else "Negative"
+            labeled_review = f"Review:\n{review}Sentiment:\n{label_text}"
+            labeled_reviews.append(labeled_review)
+        data["labeled_reviews"] = labeled_reviews
+        tokens = tokenizer(
+            data["labeled_reviews"],
             return_tensors="pt",
             max_length=config.max_sequence_length,
             padding="max_length",
             truncation=True,
         )
-    
-    def add_labels(data: Example | Batch) -> BatchEncoding:
-        CROSS_ENTROPY_IGNORE_LABEL = -100
-        pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
-        data["labels"] = torch.where(data["input_ids"] == pad_token_id, CROSS_ENTROPY_IGNORE_LABEL, data["input_ids"])
+        data["input_ids"] = tokens["input_ids"]
+        data["attention_mask"] = tokens["attention_mask"]
         return data
 
     training_dataset = (
-        load_dataset("imdb", split="train[:1000]")
+        load_dataset("imdb", split="train")
         .with_format("torch")
-        .map(tokenize, batched=True)
-        .map(add_labels, batched=True)
+        .map(preprocess, batched=True)
+        .shuffle()
+        .select(range(8000))
     )
     validation_dataset = (
-        load_dataset("imdb", split="test[:100]")
+        load_dataset("imdb", split="test")
         .with_format("torch")
-        .map(tokenize, batched=True)
-        .map(add_labels, batched=True)
+        .map(preprocess, batched=True)
+        .shuffle()
+        .select(range(1000))
     )
 
     training_dataloader = DataLoader(training_dataset, config.batch_size, pin_memory=True, shuffle=True, drop_last=True)
@@ -72,10 +84,10 @@ def main():
         bnb_4bit_compute_dtype = torch.bfloat16,
     )
     lora_config = LoraConfig(
-        r = 16,
-        lora_alpha = 16,
+        r = config.lora_rank,
+        lora_alpha = config.lora_alpha,
         target_modules = ["query", "value"],
-        lora_dropout = 0.1,
+        lora_dropout = config.lora_dropout,
         bias = "none",
     )
     model = AutoModelForCausalLM.from_pretrained(
@@ -106,6 +118,7 @@ def main():
     # training loop
     training_loss_metric = MeanMetric().to(device)
     validation_loss_metric = MeanMetric().to(device)
+    validation_accuracy = Accuracy("binary").to(device)
     accelerator.init_trackers(config.project_name, asdict(config))
 
     for step in trange(config.n_training_iterations, desc="Training", leave=True):
@@ -119,10 +132,10 @@ def main():
             output = model.forward(
                 input_ids = batch["input_ids"],
                 attention_mask = batch["attention_mask"],
-                labels = batch["labels"],
             )
-            training_loss_metric.update(output.loss)
-            accelerator.backward(output.loss / config.gradient_accumulation_steps)
+            loss = torch.nn.functional.cross_entropy(output.logits[:, -1, label_indices], batch["label"])
+            training_loss_metric.update(loss)
+            accelerator.backward(loss / config.gradient_accumulation_steps)
         optimizer.step()
         optimizer.zero_grad()
         if (step + 1) % config.log_every_n_steps == 0:
@@ -136,11 +149,20 @@ def main():
                     output = model.forward(
                         input_ids = batch["input_ids"],
                         attention_mask = batch["attention_mask"],
-                        labels = batch["labels"],
                     )
-                    validation_loss_metric.update(output.loss)
-            accelerator.log({"validation_loss": validation_loss_metric.compute()}, step)
+                    loss = torch.nn.functional.cross_entropy(output.logits[:, -1, label_indices], batch["label"])
+                    preds = torch.argmax(output.logits[:, -1, label_indices], dim=1)
+                    validation_loss_metric.update(loss)
+                    validation_accuracy.update(preds, batch["label"])
+            accelerator.log(
+                {
+                    "validation_loss": validation_loss_metric.compute(),
+                    "validation_accuracy": validation_accuracy.compute(),
+                },
+                step,
+            )
             validation_loss_metric.reset()
+            validation_accuracy.reset()
 
     accelerator.end_training()
 
@@ -153,6 +175,9 @@ class Config:
     effective_batch_size: int
     batch_size: int = field(init=False)
     learning_rate: float
+    lora_rank: int
+    lora_alpha: float
+    lora_dropout: float
     gradient_accumulation_steps: int
     max_sequence_length: int
     log_every_n_steps: int